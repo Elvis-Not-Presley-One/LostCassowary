759501a8aeeb856c63c1fb3a40534cb34f307262
Lawnguy1201 <lawnguy>
2024-08-24 12:52:18 -0400

    Added more File Handling

--- src/lostcassowary/FileHandling.java
+++ src/lostcassowary/FileHandling.java
@@ -1,78 +1,129 @@
-
 package lostcassowary;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
-/**
+/** The FileHandling class is meant to handle all of the work for the files 
  *
  * @author Lawnguy
  */
-public class FileHandling extends Region
+public class FileHandling extends Region 
 {
-      
-     private String regionFilePath;
-     private final List<String> fileNameList = new ArrayList<>();
 
-     
-     /**The getFilePath method gets the path to where the region files are located 
-      * 
-      * @return the path to the location of the region files 
-      */
-    public final String getFilePath()
-    {
-       return regionFilePath;  
-    }
-    
-    /** the setFilePath method takes the users input and sends it off to the getFilePath method 
-     * 
-     * @param initalFilePath takes in the path from the user 
+    private String regionFilePath;
+    private final List<String> fileNameList = new ArrayList<>();
+    private final List<String> parseingInfo = new ArrayList<>();
+    private final List<String> xCords = new ArrayList<>();
+    private final List<String> zCords = new ArrayList<>();
+
+    /**
+     * The getFilePath method gets the path to where the region files are
+     * located
+     *
+     * @return the path to the location of the region files
      */
-    public void setFilePath(String initalFilePath)
+    public final String getFilePath() 
     {
-     regionFilePath = initalFilePath; 
+        return regionFilePath;
     }
-    
-    /** the getFiles method obtains all of the file names in that path and stores it in an arrayList
-     * 
-     * @return all of the elements of the array containing all of the names 
+
+    /**
+     * the setFilePath method takes the users input and sends it off to the
+     * getFilePath method
+     *
+     * @param initalFilePath takes in the path from the user
      */
-    public List<String> getFiles()
+    public void setFilePath(String initalFilePath) 
     {
-     File directoryPath = new File(regionFilePath);
-     String contents[] = directoryPath.list();
-     
+        regionFilePath = initalFilePath;
+    }
+
+    /**
+     * the getFiles method obtains all of the file names in that path and stores
+     * it in an arrayList
+     *
+     * @return all of the elements of the array containing all of the names
+     */
+    public List<String> getFiles() 
+    {
+        File directoryPath = new File(regionFilePath);
+        String contents[] = directoryPath.list();
+
         System.out.println("A List of all file names in the path");
-        
-         for (String content : contents) 
-         {
-             fileNameList.add(content);
-         }
-            return fileNameList;
-    }
-    
-    /** the getFileNameParseingInfo method removes all unnecessary info from the file name 
-     * 
-     * @return the array with all of the names of the files without the clunky info 
-     */
-    public List<String> getFileNameParseingInfo()
-    {
-    
-        List<String> parseingInfo = new ArrayList<>();
-        
-        for (String fileName : fileNameList)
+
+        for (String content : contents) 
         {
-            String paresedInfo = fileName.replaceAll("r.,mca", "");
+            fileNameList.add(content);
+        }
+        return fileNameList;
+    }
+
+    /**
+     * the getFileNameParseingInfo method removes all unnecessary info from the
+     * file name
+     *
+     * @return the array with all of the names of the files without the clunky
+     * info
+     */
+    public List<String> getFileNameParseingInfo() 
+    {
+
+        //List<String> parseingInfo = new ArrayList<>();
+        for (String fileName : fileNameList) 
+        {
+            String paresedInfo = fileName.replaceAll("[r.,mca]", " ");
             parseingInfo.add(paresedInfo);
         }
-        
+
         return parseingInfo;
-        
-        
+
     }
-    
-    
-    
+
+    /**
+     * The getTokens method gets the gets the tokens and splits them up into x
+     * and z string numbers
+     *
+     */
+    public void getTokens() 
+    {
+
+        for (String parsedString : parseingInfo) 
+        {
+            StringTokenizer cookie = new StringTokenizer(parsedString, " ");
+
+            if (cookie.hasMoreTokens()) 
+            {
+                xCords.add(cookie.nextToken());
+            }
+
+            if (cookie.hasMoreTokens()) 
+            {
+                zCords.add(cookie.nextToken());
+            }
+
+        }
+    }
+
+    /**
+     * The getXCords method gets the x cords array
+     *
+     * @return the populated array
+     */
+    public List<String> getXCords() 
+    {
+        return xCords;
+    }
+
+    /**
+     * the getZCords method gets the z cords array
+     *
+     * @return the populated array
+     */
+    public List<String> getZCords()
+    {
+        return zCords;
+    }
+
 }
-
-
--- src/lostcassowary/LostCassowary.java
+++ src/lostcassowary/LostCassowary.java
@@ -1,9 +1,9 @@
-
 package lostcassowary;
 
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.List;
+
 /**
  *
  * @author Lawnguy
@@ -13,70 +13,76 @@ public class LostCassowary {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        
-     Scanner input = new Scanner(System.in);  
-     Region region = new Region();
-     FileHandling files = new FileHandling();
-        
-    String endProgram = "Cancel";
-    String regionCordCalc = "Yes";
-     
-     // Progam loop will allow to end the program at any time the user wants 
-     while (true)
-     {
-         // user input 
-         System.out.println("To stop the program at anytime type: Cancel"); 
-         System.out.println("If not enter Continue");
-         System.out.println("--------------------------------");
-         
-         String userInput = input.nextLine();
-         
-         // if the user enters cencel, the program will end 
-         if (userInput.equals(endProgram))
-         {
-             System.out.println("Program has been added");
-             break;
-         }
-         
-         // Ask the user if they want to calculate the region file cord 
-         System.out.println("\n------------------------------------");
-         System.out.println("Would you like to calculate Region Cords"); 
-         System.out.println("Enter Yes for yes, Enter No for No");
-         System.out.println("\n------------------------------------");
-         String userREgionInput = input.nextLine();
-         
-         // if they say yes go through this block 
-         if (userREgionInput.equals(regionCordCalc))
-         {
-             System.out.println("\n------------------------------------");
-             System.out.println("Enter Your X Cord Then Y Cord");
-             int userXInputs = input.nextInt();
-             int useryInputs = input.nextInt();
+    public static void main(String[] args) 
+    {
 
-             region.setRegionCords(userXInputs,useryInputs);
-             int[] strongMan = region.getRegionCords();
-             
-             // did not know array class had a tostring method built in
-             System.out.println(Arrays.toString(strongMan));
-             
-             System.out.println("\n\n---------------------------------");
-         }
-         
-         System.out.println("Please copy and Paste your Path to the regionFiles");
-         files.setFilePath(input.nextLine());
-         
-       //  System.out.println("File path: " + files.getFilePath());
-         
-         List<String> fileNames = files.getFiles(); // Now stores file names in the list
-       
-        System.out.println(fileNames);
-    
-         System.out.println(files.getFileNameParseingInfo());
-        
-        
-        
-        
-     }
+        Scanner input = new Scanner(System.in);
+        Region region = new Region();
+        FileHandling files = new FileHandling();
+
+        String endProgram = "Cancel";
+        String regionCordCalc = "Yes";
+
+        // Progam loop will allow to end the program at any time the user wants 
+        while (true) 
+        {
+            // user input 
+            System.out.println("To stop the program at anytime type: Cancel");
+            System.out.println("If not enter Continue");
+            System.out.println("--------------------------------");
+
+            String userInput = input.nextLine();
+
+            // if the user enters cencel, the program will end 
+            if (userInput.equals(endProgram)) 
+            {
+                System.out.println("Program has been added");
+                break;
+            }
+
+            // Ask the user if they want to calculate the region file cord 
+            System.out.println("\n------------------------------------");
+            System.out.println("Would you like to calculate Region Cords");
+            System.out.println("Enter Yes for yes, Enter No for No");
+            System.out.println("\n------------------------------------");
+            String userREgionInput = input.nextLine();
+
+            // if they say yes go through this block 
+            if (userREgionInput.equals(regionCordCalc)) 
+            {
+                System.out.println("\n------------------------------------");
+                System.out.println("Enter Your X Cord Then Y Cord");
+                int userXInputs = input.nextInt();
+                int useryInputs = input.nextInt();
+
+                region.setRegionCords(userXInputs, useryInputs);
+                int[] strongMan = region.getRegionCords();
+
+                // did not know array class had a tostring method built in
+                System.out.println(Arrays.toString(strongMan));
+
+                System.out.println("\n\n---------------------------------");
+            }
+
+            System.out.println("Please copy and Paste your Path to the regionFiles");
+            files.setFilePath(input.nextLine());
+
+            //  System.out.println("File path: " + files.getFilePath());
+            List<String> fileNames = files.getFiles(); // Now stores file names in the list
+            System.out.println("---------------------------------\n------------------------------");
+
+            System.out.println(fileNames);
+
+            files.getFileNameParseingInfo();
+            System.out.println(files.getFileNameParseingInfo());
+
+            files.getFileNameParseingInfo();
+            System.out.println("---------------------------------\n------------------------------");
+            System.out.println(files.getZCords());
+            System.out.println(files.getZCords());
+            System.out.println("---------------------------------\n------------------------------");
+
+
+        }
     }
 }
