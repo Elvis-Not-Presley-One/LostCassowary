8f9ceaa55cd4bef64bbba4e8d0ed211cbe34fa96
Lawnguy1201 <lawnguy1201@gmail.com>
2024-09-22 20:20:20 -0400

    Lib lv tag is removed

--- src/lostcassowary/Chunks.java
+++ src/lostcassowary/Chunks.java
@@ -7,12 +7,16 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import net.querz.mca.Chunk;
 import net.querz.mca.MCAFile;
 import net.querz.mca.MCAUtil;
 import net.querz.nbt.io.NBTUtil;
 import net.querz.nbt.io.NamedTag;
+import net.querz.nbt.tag.ByteTag;
 import net.querz.nbt.tag.CompoundTag;
+import net.querz.nbt.tag.DoubleTag;
 /**
  *
  * @author Lawnguy
@@ -30,26 +34,42 @@ public class Chunks extends Region {
         Object[] chunkOffsets = super.getChunkLocationOffset().toArray();
         Object[] filename = getFiles().toArray();
         
-        System.out.println(chunkOffsets.length);
-        System.out.println(filename.length);
-
+       // System.out.println(chunkOffsets.length);
+       // System.out.println(filename.length);
+        int counter = 0; 
         int fileBeingUsed = 0;
-        
-        for (int i = 0; i < chunkOffsets.length; i++) 
+        while (fileBeingUsed != filename.length)
         {
-            if (chunkOffsets[i].equals(8192)) 
+            if (chunkOffsets[counter].equals(8192)) 
             {
                fileBeingUsed++; 
             }
+           System.out.println((File) filename[fileBeingUsed]);
+
+           //NamedTag namedTag = NBTUtil.read((File) filename[fileBeingUsed]);
+           MCAFile mcaFile = MCAUtil.read((File) filename[fileBeingUsed]);
            
-           // NamedTag namedTag = NBTUtil.read((File) filename[fileBeingUsed]);
-           // MCAFile mcaFile = MCAUtil.read((File) filename[fileBeingUsed]);
-            
-           // System.out.println(namedTag.getTag().toString());
-           // System.out.println(namedTag.getName());
-           // System.out.println(namedTag.toString());
+          for (int x = 0; x < 32; x++)
+           {
+               for (int z = 0; z < 32; z++)
+               {
+                    
+               Chunk chunk = mcaFile.getChunk(x, z);
+               if (chunk == null)
+               {
+                   System.out.println("chunk is null at" + x + z);
+                   continue;
+               }
+                   CompoundTag chunkNBT = chunk.getHeightMaps();
+               System.out.println(chunkNBT);
+               }
+           }
+           
+           //System.out.println(namedTag.getTag().toString());
+           //System.out.println(namedTag.getName());
+          // System.out.println(namedTag.toString());
           
-           
+           counter++;
         }
 
        return fileBeingUsed;
