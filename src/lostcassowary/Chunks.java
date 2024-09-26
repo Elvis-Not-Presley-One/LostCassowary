654d45f7c49e56bd66294974dec3e713912d3cb6
Lawnguy1201 <lawnguy>
2024-09-25 20:34:54 -0400

    Lib lv tag is removed

--- src/lostcassowary/Chunks.java
+++ src/lostcassowary/Chunks.java
@@ -18,68 +18,73 @@ import net.querz.nbt.tag.ByteTag;
 import net.querz.nbt.tag.CompoundTag;
 import net.querz.nbt.tag.DoubleTag;
 import net.querz.nbt.tag.ListTag;
+
 /**
  *
  * @author Lawnguy
  */
-public class Chunks extends Region {
+public class Chunks extends Region 
+{
 
-    public void setFileNames(String initalFilePath) {
+    public void setFileNames(String initalFilePath) 
+    {
         super.setFilePath(initalFilePath);
     }
-    
+
     //Chunk offset length set to 0 so nothing enters the loop 
     public int getNBTData() throws FileNotFoundException, IOException 
     {
         super.getChunkLocations();
-        Object[] chunkOffsets = super.getChunkLocationOffset().toArray();
+        //Object[] chunkOffsets = super.getChunkLocationOffset().toArray();
         Object[] filename = getFiles().toArray();
-        
-       // System.out.println(chunkOffsets.length);
-       // System.out.println(filename.length);
-       
-        int counter = 0; 
-        int fileBeingUsed = 0;
-        while (fileBeingUsed != filename.length)
-        {
-            if (chunkOffsets[counter].equals(8192)) 
-            {
-               fileBeingUsed++; 
-            }
-           System.out.println((File) filename[fileBeingUsed]);
 
-           //NamedTag namedTag = NBTUtil.read((File) filename[fileBeingUsed]);
-           MCAFile mcaFile = MCAUtil.read((File) filename[fileBeingUsed]);
-           
-          for (int x = 0; x < 32; x++)
-           {
-               for (int z = 0; z < 32; z++)
-               {
-                  
-               Chunk chunk = mcaFile.getChunk(x, z);
-               CompoundTag chunkHM = chunk.getHeightMaps();
-                   System.out.println(chunkHM);
-               
-               if (chunk == null)
-               {
-                   System.out.println("chunk is null at" + x + z);
-                   chunk.cleanupPalettesAndBlockStates();
-                   continue;
-               }
-               
-               for (int y = 0; y < 256; y++)
-               {
-                   
-                   int chunkNBT = chunk.getBiomeAt(x, y, z);
-                   System.out.println(chunkNBT);
-               }
-               }
-           }
-          
-           counter++;
+        // System.out.println(chunkOffsets.length);
+        // System.out.println(filename.length);
+        int counter = 0;
+        int fileBeingUsed = 0;
+        while (fileBeingUsed != filename.length) 
+        {
+
+            System.out.println((File) filename[fileBeingUsed]);
+
+            //NamedTag namedTag = NBTUtil.read((File) filename[fileBeingUsed]);
+            MCAFile mcaFile = MCAUtil.read((File) filename[fileBeingUsed]);
+
+            for (int x = 0; x < 32; x++) 
+            {
+                for (int z = 0; z < 32; z++) 
+                {
+                    mcaFile.cleanupPalettesAndBlockStates();
+
+                    Chunk chunk = mcaFile.getChunk(x, z);
+
+                    CompoundTag chunkHM = chunk.getHeightMaps();
+                    System.out.println(chunkHM);
+
+                    ListTag<CompoundTag> entities = chunk.getTileEntities();
+                    if (entities != null)
+                    {
+                        System.out.println(entities.asStringTagList());
+                    }
+                    else{
+                        System.out.println("No entites found in" + x + " " + z);
+                    }
+
+                    for (int y = -60; y < 256; y++) 
+                    {
+                       // System.out.println(mcaFile.getBlockStateAt(x, y, z));
+                        int biomes = mcaFile.getBiomeAt(x, y, z);
+                        System.out.println(biomes);
+                    }
+                }
+            }
+            fileBeingUsed++;
         }
 
-       return fileBeingUsed;
+        counter++;
+        return fileBeingUsed;
     }
 
 }
+
+
